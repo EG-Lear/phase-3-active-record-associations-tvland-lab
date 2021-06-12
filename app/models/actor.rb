d273e264219e1bdb4b827a42ea80d37faac19d57
Garret <gwiats@gmail.com>
2021-06-12 18:48:13 -0400

    commit tree 149e275dc7dd2b30f089cc2bb25d9c90725e22b1

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    char_name = characters.find_by(actor_id: self.id).name
+    show_id = characters.find_by(actor_id: self.id).show_id
+    show_name = shows.find(show_id).name
+    final = ["#{char_name} - #{show_name}"]
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    actor_id = characters.find_by(show_id: self.id).actor_id
+    actor_first = actors.find(actor_id).first_name
+    actor_last = actors.find(actor_id).last_name
+    ["#{actor_first} #{actor_last}"]
+  end
 end
